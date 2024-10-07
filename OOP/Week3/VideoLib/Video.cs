a602702e774ac9a5548c0269850d937997896934
drmsbgr <drmsbgr@gmail.com>
2024-10-07 10:41:24 +0300

    folders has been renamed

---
+++ OOP/Week3/VideoLib/Video.cs
@@ -0,0 +1,9 @@
+﻿namespace VideoLib;
+
+public class Video
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public float Duration { get; set; }
+    public List<string>? Keywords { get; set; }
+}
